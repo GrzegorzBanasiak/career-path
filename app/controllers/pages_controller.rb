4d55a317e94363c7a162f65439f8756950d1259e
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2018-09-23 11:07:55 +0200

    create panel for users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!
   layout "mainpage", only: [:index]
 
-  def index;end
+  def index
+      go_to_panel_if_user_signed_in
+  end
+
   def about;end
+
+  private
+
+    def go_to_panel_if_user_signed_in
+      if user_signed_in?
+        redirect_to panel_index_path
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/about'
+  get 'panel/index'
+
 end
