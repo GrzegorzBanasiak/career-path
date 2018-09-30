b946c912edc9e819138076ecb9b2f62858f2fea9
GrzegorzBanasiak <grzegorzbanasiak95@gmail.com>
2018-10-01 00:04:16 +0200

    add validates for forms

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,15 @@
 class Form < ApplicationRecord
   belongs_to :user
   has_many :questions
-  
+
+  validates :position,
+            presence: true,
+            length: {minimum: 5, maximum: 150}
+  validates :location,
+            presence: true,
+            length: {minimum: 5, maximum: 150}
+  validates :company,
+            presence: true,
+             length: {minimum: 5, maximum: 150} 
+
 end
